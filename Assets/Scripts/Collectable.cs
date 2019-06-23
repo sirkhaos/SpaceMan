de095a13094ba5b151958e7107ccf1aba35b7c93
sirkhaos <alberto.torres.carreno@gmail.com>
2019-06-22 21:01:03 -0400

    diseñando script de collectable

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollectableType
+{
+    HealthPotion,
+    ManaPotion,
+    Money
+}
+
+public class Collectable : MonoBehaviour
+{
+    public CollectableType type = CollectableType.Money;
+    public int value = 1;
+
+
+    private SpriteRenderer sprite;
+    private CircleCollider2D itemCollider;
+
+    bool hasBeenCollected = false;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        itemCollider = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player"))
+        {
+            Collect();
+        }
+    }
+
+    private void Show()
+    {
+        sprite.enabled = true;
+        itemCollider.enabled = true;
+        hasBeenCollected = false;
+    }
+
+    private void Hide()
+    {
+        sprite.enabled = false;
+        itemCollider.enabled = false;
+    }
+
+    private void Collect()
+    {
+        Hide();
+        hasBeenCollected = true;
+
+        switch (this.type)
+        {
+            case CollectableType.Money:
+                //TODO: logica de la moneda 
+                break;
+            case CollectableType.HealthPotion:
+                //TODO: logica de la posion de vida
+                break;
+            case CollectableType.ManaPotion:
+                //TODO: logica de la posion de mana
+                break;
+        }
+    }
+}
